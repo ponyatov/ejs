a685153b67bd5999d83408c81c057f1ee71c6524
Dmitry Ponyatov <dponyatov@gmail.com>
2025-09-12 22:53:18 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ arch/cortexm/cortexm.mk
@@ -0,0 +1,6 @@
+OS     = none
+TARGET = arm-none-eabi
+EXE    = .elf
+
+APT   += gcc-arm-none-eabi gdb-multiarch qemu-system-arm
+APT   += newlib-source stlink-tools dfu-util dos2unix stm32flash
---
+++ arch/i386/i386.mk
@@ -0,0 +1,4 @@
+ TARGET  = i686-linux-gnu
+RTARGET  = i686-unknown-linux-gnu
+OS       = linux
+APT     += qemu-system-x86 gcc-i686-linux-gnu gdb-multiarch
